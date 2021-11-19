3fe90e6d337399f0d631445df3eb6e4fa1f13d38
NaylsonFerreira <naylson.ferreira@fiberwork.net>
2021-11-19 08:37:45 -0300

    plot feat1

--- src/pages/Dashboard/chart.js
+++ src/pages/Dashboard/chart.js
@@ -2,25 +2,65 @@ import React, { useRef, useEffect } from 'react'
 
 const Canvas = props => {
 
-    const canvasRef = useRef(null)
+    const canvasRef = useRef(null);
+    const canvasStyle = {
+        backgroundColor: 'green',
+        width: '100%',
+    }
 
-    const draw = (ctx) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        for (let i = 0; i < 10; i++) {
-            const mov = (i * 100)
-            ctx.fillRect(mov, i * 5, 100, 100);
+    const draw = (canvas) => {
+        const context = canvas.getContext('2d')
+        // context.imageSmoothingQuality = "high";
+        context.imageSmoothingEnabled = false;
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        // const [screenWidth, screenHeight] = [canvas.clientWidth, canvas.clientHeight];
+
+        const [canais, tempo] = [300, 50];
+        const r1 = generateFrame(canais, tempo);
+        // 300x148
+
+        const w = 300 / canais;
+        const h = 150 / tempo;
+        // context.fillRect(0, 0, w, h);
+        // context.fillRect(50 + 5, 0, w, h);
+        // context.fillRect(100 + 10, 0, w, h);
+        // context.fillRect(150 + 15, 0, w, h);
+        for (let j = 0; j < tempo; j++) {
+            const py = h * j;
+            for (let i = 0; i < canais; i++) {
+                const px = (w * i);
+                let x = "rgb(0,0," + r1[j][i] + ")";
+                context.fillStyle = x;
+                context.fillRect(px, py, w, h);
+            }
         }
     }
 
+    const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    const generateFrame = (canais, tempo) => {
+        const data = [];
+        for (let j = 0; j < tempo; j++) {
+            const line = [];
+            for (let i = 0; i < canais; i++) {
+                const x = getRandomInt(0, 254);
+                line.push(x);
+            }
+            data.push(line);
+        }
+        return data;
+    }
+    const canvas = canvasRef.current;
     useEffect(() => {
+        
+    }, []);
 
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        draw(context);
-
-    }, [draw])
-
-    return <canvas ref={canvasRef} {...props} />
+    setInterval(() => canvas ? draw(canvas) : () => { }, 1000);
+    return <canvas ref={canvasRef} {...props} style={canvasStyle} />
 }
 
 export default Canvas
